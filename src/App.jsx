117a542a1f7d0547ad1ec4a05d9712e2d71259e9
SheikhNegarSultana <sultanashashi64@gmail.com>
2024-10-29 21:41:15 +0600

    Tailwind and DaisyUi Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1 className=' mt-[4rem] font-extrabold text-[2rem] text-center text-pretty'> Price Options</h1>
+      
+    </>
+  )
+}
+
+export default App
