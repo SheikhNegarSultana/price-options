f210cda55d520ef94fa77fc14e300060730e4093
SheikhNegarSultana <sultanashashi64@gmail.com>
2024-11-01 18:47:42 +0600

    React Awesome Components

--- src/Components/Phones/Phones.jsx
+++ src/Components/Phones/Phones.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { BarChart, Bar, ResponsiveContainer,XAxis, YAxis  } from 'recharts';
-
+import { Audio } from 'react-loader-spinner';
 const Phones = () => {
     const [phone, setPhone] = useState([]);
+    const [load , setLoad] = useState(true);
 
     useEffect(() => {
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -17,12 +18,22 @@ const Phones = () => {
                     return obj;
                 });
                 setPhone(fakePhoneObj);
+                setLoad(false)
             })
             
     }, []);
 
     return (
         <div>
+{ load && <Audio
+  height="80"
+  width="80"
+  radius="9"
+  color="green"
+  ariaLabel="three-dots-loading"
+  wrapperStyle
+  wrapperClass
+/>}
             <h1>Total Phone: {phone.length}</h1>
             <ResponsiveContainer width="100%" height={300}>
                 <BarChart data={phone}>
